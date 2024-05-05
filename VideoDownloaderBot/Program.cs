4ebff897234b5f60f29cb32cdb655ff5cdfc5f9a
Xoggas <sashakor16@gmail.com>
2024-05-06 02:03:13 +0300

    Removed infinite waiting

--- VideoDownloaderBot/Program.cs
+++ VideoDownloaderBot/Program.cs
@@ -1,15 +1,5 @@
 using Xoggas.VideoDownloaderBot;
 
-var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-
-WebApplication app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
-app.Run($"http://0.0.0.0:{port}");
-
 var token = Environment.GetEnvironmentVariable("TELEGRAM_BOT_TOKEN");
 
 if (string.IsNullOrEmpty(token))
@@ -21,4 +11,12 @@ var bot = new Bot(token);
 
 bot.Start();
 
-await Task.Delay(-1);
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+WebApplication app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run($"http://0.0.0.0:{port}");
